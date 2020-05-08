88dec4a2dd86fb88ecdd52bb7a211500b9d6b725
sethdroberts <sethknock@gmail.com>
2020-05-08 01:05:44 -0700

    Imported relevant algo fcns from old code

--- charles_algo.py
+++ charles_algo.py
@@ -1,400 +1,95 @@
-#Requires python3.6 or above
-#Run the following commands to install modules:
-    #pip3 install alpaca_trade_api
-    #pip3 install websocket-client
-
-import alpaca_trade_api as trade_api 
-import time
-import datetime 
 import websocket
 import json
 import config as cfg
-from sendsms import SendSMS as sms
 import logging
+import scraper as s
+import charles_functions as cf
 
-logging.basicConfig(filename='darwin.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
-
-APCA_API_KEY_ID = cfg.APCA_API_KEY_ID
-APCA_API_SECRET_KEY = cfg.APCA_API_SECRET_KEY
-APCA_API_BASE_URL = cfg.APCA_API_BASE_URL
-ACCOUNT_URL = cfg.ACCOUNT_URL
-
-POLYGON_API_KEY = APCA_API_KEY_ID
-SOCKET = cfg.SOCKET
-
-SECURITY = cfg.SECURITIES
-CHANNEL = cfg.CHANNEL
-TPROFIT = cfg.TPROFIT
-SLOSS = cfg.SLOSS
-EQ_PCT = 1/len(SECURITY)
-TICKERS = ''
-
-#Assemble tickers
-a = 0
-while a < len(SECURITY):
-    TICKERS = TICKERS + CHANNEL + '.' + SECURITY[a]
-    if a == len(SECURITY) - 1:
-        a = a + 1
-    else:
-        TICKERS = TICKERS + ','
-        a = a + 1
-
-api = trade_api.REST(APCA_API_KEY_ID,\
-APCA_API_SECRET_KEY,\
-APCA_API_BASE_URL)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('charles.log')
+logger.addHandler(file_handler)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler.setFormatter(formatter)
 
 #Dictionary structure
-clock = api.get_clock()
-start_time = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
 day_perform = []
 current_positions = []
 initiation = {}
 top_change = []
 
-#Returns seconds till market closes. Delayed by 1 seconds to prevent excessive API calls (only after portfolio built)
-def time_till_close():
-    clock = api.get_clock()
-    closing_time = clock.next_close.replace(tzinfo=datetime.timezone.utc).timestamp()
-    curr_time = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
-    time_till_close = int(closing_time - curr_time)
-    time.sleep(1)
-    return time_till_close
+#WARNING: Don't change this list without changing order_from_list function. It's default for all order inputs:
+order_keys = ['symbol', 'qty', 'profit_price', 'loss_price']
 
-def time_till_open():
-    clock = api.get_clock()
-    open_time = clock.next_open.replace(tzinfo=datetime.timezone.utc).timestamp()
-    curr_time = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
-    time_till_open = int(open_time - curr_time)
-    return time_till_open
+#Clear all dictionaries
+def clear_all():
+    cf.clear(day_perform)
+    cf.clear(current_positions)
+    cf.clear(initiation)
+    cf.clear(top_change)
 
-def dictionary_filled():
-    count = 0
-    d = 0
-    while d < len(day_perform):
-        stock_perform = day_perform[d]
-        price = stock_perform.get('current_price')
-        if price == 0:
-            status = 0
-        else:
-            status = 1
-        count = count + status
-        d = d + 1
-    if count == len(day_perform):
-        return True
-    else:
-        return False
-
-def place_order(security, qty, side):
-    api.submit_order(
-    symbol = security,
-    qty = qty,
-    side = side,
-    type = 'market',
-    time_in_force = 'gtc')
-    time.sleep(2)
-    logging.debug('Placed order successfully')
-
-def build_portfolio():
-    g = 0
-    while g < len(current_positions):
-        logging.debug(g)
-        order_data = current_positions[g]
-        symbol = order_data.get('symbol')
-        profit_price = float(order_data.get('profit_price'))
-        loss_price = float(order_data.get('loss_price'))
-        qty = int(order_data.get('qty'))
-        logging.debug(symbol, profit_price, loss_price, qty)
-
-        place_order(symbol, qty, 'buy')
+#Places an order based on a list of data
+def order_from_list(list, side):
+    symbol = list[0]
+    qty = list[1]
+    profit_price = list[2]
+    loss_price = list[3]
+    cf.place_order(symbol, qty, side)
+    if side == 'buy':
         logging.info('Ordered {} shares of {}'.format(qty,symbol))
         logging.info('Take profit at {}. Stop loss at {}.'.format(profit_price,loss_price))
-        g = g + 1
+    else:
+        logging.info('Liquidated {} shares of {}'.format(qty,symbol))
 
-#Takes a dictionary and creates a bracket order for that
-def change_position(data):
-    security = data.get('symbol')
-    logging.debug(security)
-    profit_price = float(data.get('profit_price'))
-    loss_price = float(data.get('loss_price'))
-    qty = int(data.get('qty'))
-    logging.debug(security, profit_price, loss_price, qty)
-    place_order(security, qty, 'buy')
+#Places order based on dictionary and list of keys to extract
+def change_position(dict, keys_list):
+    order_data = cf.multiple_listdict(dict, keys_list)
+    symbol = order_data[0]
+    order_from_list(order_data, 'buy')
+    logging.info("==Equity transfer to {} complete==".format(symbol))
 
-    logging.info('Ordered {} shares of {}'.format(qty,security))
-    logging.info('Take profit at {}. Stop loss at {}.'.format(profit_price,loss_price))
-    logging.info("==Equity transfer to {} complete==".format(security))
+#Places orders for all lists of order variables in list of lists
+def build_portfolio(list, keys_list):
+    order_list = cf.build_values_list(list, keys_list)
+    x = 0
+    while x < len(order_list):
+        order_data = order_keys[x]
+        order_from_list(order_data, 'buy')
+    x += 1
 
-#Liquidates a position from a position_data file
-def liquidate_position(data):
-    security = data.get('symbol')
-    logging.debug(security)
-    qty = int(data.get('qty'))
-    logging.debug(security, qty)
-    place_order(security, qty, 'sell')
-
-    logging.info('Liquidated {} shares of {}'.format(qty,security))
-
-def performance():
-    account = api.get_account()
-    balance = float(account.equity)
-    last_balance = float(account.last_equity)
-    balance_change = round(balance - last_balance,2)
-    percentage_change = round(((balance - last_balance)/last_balance * 100),2)
-    return "Today's balance change: ${}. Today's percentage change: {}%".format(balance_change, percentage_change)
-
+#Liquidates a position from a dictionary item
+def liquidate_position(dict, keys_list):
+    order_data = cf.multiple_listdict(dict, keys_list)
+    order_from_list(order_data, 'sell')
+    
 def on_open(ws):
     auth_data = {
     "action":"auth",
-    "params":POLYGON_API_KEY
+    "params":cfg.POLYGON_API_KEY
     }
 
     ws.send(json.dumps(auth_data))
 
     channel_data = {
     "action": "subscribe",
-    "params": TICKERS
+    "params": s.assemble_tickers(cfg.SECURITIES)
     }
 
     ws.send(json.dumps(channel_data))
 
-    #Clear any existing dictionary data
-    day_perform.clear()
-    current_positions.clear()
-    top_change.clear()
-    initiation.clear()
-
-    #Assembles initial dictionaries for each security
-    b = 0
-    while b < len(SECURITY):
-        #Security list, day_perform, and current_positions are all in the same order
-        day_perform.append({'symbol': SECURITY[b],'open_price': 0,'current_price': 0, 'day_change': 0})
-        current_positions.append({'symbol': SECURITY[b], 'start_price': 0, 'profit_price': 0, 'loss_price': 0, 'qty': 0, 'equity': 0, 'status': 0})
-        b = b + 1
-    top_change.append({'symbol': SECURITY[0], 'change':0.01, 'price': 0})
-    top_change.append({'symbol': SECURITY[1], 'change':0.001, 'price': 0})
-    initiation.update({'initiation': 0, 'iteration': 0})
-    #Liquidate any existing orders or positions
-    api.cancel_all_orders()
-    api.close_all_positions()
-    time.sleep(2)
-    logging.debug("==Liquidated all existing stock positions==")
+    clear_all()
 
 def on_message(ws, message):
-    global current_positions, top_change, initiation
-    #Ensures no iterations during launch
-    target_iterations = len(SECURITY) + 2
-    count = initiation.get('iteration')
-    if count < target_iterations:
-        logging.debug(message)
-        count = count + 1
-        initiation.update({'iteration': count})
-
-    else:
-        #Update day_perform dictionary with current price and day change
-        c = 0
-        while c < len(SECURITY):
-            security = SECURITY[c]
-            data = json.loads(message)
-            data = data[0]
-            if data.get('sym') == security:
-                stock_perform = day_perform[c]
-                current_price = data.get('c')
-                stock_perform.update({'current_price': current_price})
-                if initiation.get('initiation') == 0:
-                    stock_perform.update({'open_price': current_price})
-                open_price = stock_perform.get('open_price')
-                day_change = (current_price - open_price)/open_price * 100
-                stock_perform.update({'day_change': day_change})
-
-                #Update top_change list
-                change_top = top_change[0]
-                change_second = top_change[1]
-                if security == change_top.get('symbol'):
-                    change_top.update({'symbol': security, 'change': day_change, 'price': current_price})
-                elif security == change_second.get('symbol'):
-                    if day_change > change_top.get('change'):
-                        change_second.update(change_top)
-                        change_top.update({'symbol': security, 'change': day_change, 'price': current_price})
-                    else:
-                        change_second.update({'symbol': security, 'change': day_change, 'price': current_price})
-                elif day_change > change_top.get('change'):
-                    change_second.update(change_top)
-                    change_top.update({'symbol': security, 'change': day_change, 'price': current_price})
-                elif day_change > change_second.get('change'):
-                    change_second.update({'symbol': security, 'change': day_change, 'price': current_price})
-            c = c + 1
-
-        #Ensures we don't start without dictionaries filled
-        if dictionary_filled() == False:
-            logging.debug('==Loading dictionaries===')
-
-        elif initiation.get('initiation') == 0:
-            account = api.get_account()
-            equity = account.equity
-            equity = float(equity)
-            available = equity*EQ_PCT
-
-            #Create initial positions in dictionary
-            y = 0
-            while y < len(current_positions):
-                position_data = current_positions[y]
-                perform_data = day_perform[y]
-                price = float(perform_data.get('current_price'))
-
-                profit_price = float(price*TPROFIT)
-                loss_price = float(price*SLOSS)
-                qty = int(available // price)
-
-                position_data.update({'start_price': price})
-                position_data.update({'profit_price': profit_price})
-                position_data.update({'loss_price': loss_price})
-                position_data.update({'qty': qty})
-                position_data.update({'equity': available})
-                position_data.update({'status': 1})
-                logging.debug(position_data)
-                y = y + 1
-            logging.debug(current_positions)
-            #Place orders for all initial positions
-            if y == len(current_positions):
-                logging.info('==Building portfolio==')
-                build_portfolio()
-                initiation.update({'initiation':1})
-                logging.info('==Portfolio build completed==')
-        elif time_till_close() > (60*35):
-            #Check for prices beneath stoplosses
-            logging.debug(top_change)
-            e = 0
-            while e < len(SECURITY):
-                position_data = current_positions[e]
-                perform_data = day_perform[e]
-                security = position_data.get('symbol')
-
-                price = float(perform_data.get('current_price'))
-                loss_price = position_data.get('loss_price')
-                profit_price = position_data.get('profit_price')
-                status = position_data.get('status')
-                logging.debug(security,price, profit_price, loss_price, status)
-
-                if price > profit_price and status < 2:
-                    logging.info('=={} hit 1%. Liquidating position=='.format(security))
-                    liquidate_position(position_data)
-                    position_data.update({'status': 2}) #Just to make sure the algo doesn't miss it if the price 
-                    status = position_data.get('status')
-                    logging.debug('Status is now {}'.format(status))
-                    logging.info('==Liquidation complete==')
-                    # profit_price = price*TPROFIT
-                        # loss_price = price*SLOSS
-                        # position_data.update({'start_price': price})
-                        # position_data.update({'profit_price': profit_price})
-                        # position_data.update({'loss_price': loss_price})
-                        # logging.debug('==Hit take_profit for {}. Adjusting position data=='.format(security))
-
-
-                #Find top-performing stock and move equity there. Update dictionary.
-                elif price < loss_price or status == 0:
-                    logging.debug('Position status of {} is {}'.format(security, status))
-                    if status == 1:
-                        liquidate_position(position_data)
-                        position_data.update({'status': 0}) #Just to make sure the algo doesn't miss it if the price 
-                        status = position_data.get('status')
-                        logging.debug('Status is now {}'.format(status))
-
-                    elif status == 0:
-                        symbol = change_top.get('symbol')
-                        change_top = top_change[0]
-                        change_second = top_change[1]
-
-                        logging.debug("This stock is {}. Top stock is {}. Top stock's change: {}. Second stock's change: {}".format(security, symbol, change_top.get('change'), change_second.get('change')))
-                        
-                        #Skip equity change if top stock isn't >0.1%. Prevents repeated stoploss.
-                        if symbol == security and change_second.get('change') < 0.1:
-                            logging.debug('==No alternative stocks above 0.0 percent for {}, skipping stoploss check=='.format(security))
-
-                        elif change_top.get('change') < 0.1:
-                            logging.debug('==No alternative stocks above 0.0 percent for {}, skipping stoploss check=='.format(security))
-                        else:
-                            available = float(position_data.get('equity'))
-                            logging.debug(available)
-
-                            change_top = top_change[0]
-                            
-                            if symbol == security:
-                                change_second = top_change[1]
-                                symbol = change_second.get('symbol')
-                                logging.info('=={} fell below stoploss. Is already top stock. Moving equity to {}=='.format(security, symbol))
-                                price = float(change_second.get('price'))
-                            else:
-                                logging.info('=={} fell below stoploss, moving equity to {}=='.format(security, symbol))
-                                price = float(change_top.get('price'))
-                            profit_price = price*TPROFIT
-                            loss_price = price*SLOSS
-                            logging.debug(symbol, price)
-                            qty = int(available // price)
-                            logging.debug(qty)
-                            logging.debug(symbol, profit_price, loss_price, qty, price)
-
-                            position_data.update({'symbol': symbol})
-                            position_data.update({'start_price': price})
-                            position_data.update({'profit_price': profit_price})
-                            position_data.update({'loss_price': loss_price})
-                            position_data.update({'qty': qty})
-                            position_data.update({'equity': available})
-                            position_data.update({'status': 1})
-                            logging.debug(position_data)
-                            logging.debug(current_positions)
-                            change_position(position_data)
-                    else:
-                        pass
-                e = e + 1
-        else:
-            logging.info("Market closing soon. Closing positions.")
-            api.cancel_all_orders()
-            api.close_all_positions()
-            time.sleep(2)
-            
-            logging.info('===Final stock positions==')
-            h = 0
-            while h < len(current_positions):
-                logging.info(current_positions[h])
-                h = h + 1
-            logging.info('==========================')
-            result = performance()
-            logging.info(result)
-            sms.send_sms(result)
-
-            day_perform.clear()
-            current_positions.clear()
-            top_change.clear()
-            initiation.clear()
-
-            logging.info('==Shutting down trading algorithm for the day===')
-            time_to_close = time_till_close()
-            time.sleep(time_to_close + 60)
-            activate_algo()
+    logger.info(message)
 
 def on_error(ws, error):
-    logging.warning(error)
+    logger.warning(error)
 
 def on_close(ws):
-    logging.critical('### closed ###')
+    logger.critical('### closed ###')
 
-ws = websocket.WebSocketApp(SOCKET,
+ws = websocket.WebSocketApp(cfg.SOCKET,
                             on_open = on_open,
                             on_message = on_message,
                             on_error = on_error,
                             on_close = on_close)
-
-def activate_algo():
-    if clock.is_open:
-        ws.run_forever()
-
-    else:
-        time_to_open = time_till_open()
-        logging.info('==Market is closed! It will open in {} seconds=='.format(time_to_open))
-        time.sleep(time_to_open + 60)
-        #Also add web scraper here and add securities. It will refresh every day.
-        logging.info('==Launching trading algorithm==')
-        ws.run_forever()
-
-if __name__=="__main__":
-    activate_algo()
--- charles_functions.py
+++ charles_functions.py
@@ -2,39 +2,177 @@
 import config as cfg
 import datetime
 import time
-    
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('charles.log')
+logger.addHandler(file_handler)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler.setFormatter(formatter)
+
+#Places an Alpaca order with certain input criteria
+def place_order(security, qty, side):
+    api = cfg.api
+    api.submit_order(
+    symbol = security,
+    qty = qty,
+    side = side,
+    type = 'market',
+    time_in_force = 'gtc')
+    time.sleep(2)
+    logger.debug('==Placed order successfully==')
+
+def performance():
+    api = cfg.api
+    account = api.get_account()
+    balance_change = round(float(account.equity) - float(account.last_equity),2)
+    percentage_change = round(((float(account.equity) - float(account.last_equity))/float(account.last_equity) * 100),2)
+    return "Today's balance change: ${}. Today's percentage change: {}%".format(balance_change, percentage_change)
+
+def liquidate_all():
+    api = cfg.api
+    api.cancel_all_orders()
+    api.close_all_positions()
+    time.sleep(2)
+    logging.debug("==Liquidated all existing stock positions==")
+
+#Returns get_clock
 def get_clock():
     api = cfg.api
     return api.get_clock()
 
+#True if market is open; else False
 def clock_is_open():
     clock = get_clock()
     return clock.is_open
 
+#Returns current time in seconds
 def curr_time():
     clock = get_clock()
     curr_time = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
     return curr_time
 
+#Returns next closing time in seconds
 def closing_time():
     clock = get_clock()
     closing_time = clock.next_close.replace(tzinfo=datetime.timezone.utc).timestamp()
     return closing_time
 
+#Returns next open time in seconds
 def open_time():
     clock = get_clock()
     open_time = clock.next_open.replace(tzinfo=datetime.timezone.utc).timestamp()
     return open_time
 
+#Returns time till next close in seconds
 def time_till_close():
     time_till_close = closing_time() - curr_time()
     return time_till_close
 
+#Returns time till next open in seconds
 def time_till_open():
     time_till_open = open_time() - curr_time()
     return time_till_open
 
-    
+#Clears a list or dictionary
+def clear(list):
+    list.clear()
 
-print(time_till_open())
-print(time_till_close())
+#Returns the value of a key in a dictionary
+def dict_val(dict, key):
+    return dict.get(key)
+
+#Returns the value of a key in a dictionary at an index # of a list
+def get_listdict_val(list, index, key):
+    dict = list[index]
+    return dict_val(dict, key)
+
+#Returns 0 if value = 0, or 1 if nonzero
+def non_zero(value):
+    if value == 0:
+        return 0
+    else:
+        return 1
+
+#Returns True if all dictionaries in a list have a non-zero value for a key
+def dictionary_filled(list, key):
+    count = 0
+    x = 0
+    while x < len(list):
+        value = get_listdict_val(list, x, key)
+        status = non_zero(value)
+        count += status
+        x = x + 1
+    if count == len(list):
+        return True
+    else:
+        return False
+
+#Returns the values of all keys in keys_list in a given dictionary
+def multiple_listdict(dict, keys_list):
+    values_list = []
+    x = 0
+    while x < len(keys_list):
+        value = dict_val(dict, keys_list[x])
+        values_list.append(value)
+        x += 1
+    return values_list
+
+#Takes a list of dictionaries and a list of keys to be found
+#Returns a list of lists with the key values for each item
+def build_values_list(list, keys_list):
+    list_of_lists = []
+    x = 0
+    while x < len(list):
+        values = multiple_listdict(list[x], keys_list)
+        list_of_lists.append(values)
+        x = x + 1
+    return list_of_lists
+
+
+
+# stuff = {'item': 3, 'bottle': 'string'}
+# things = {'item': 2, 'bottle': 52.0}
+# others = {'item': 6, 'bottle': 2}
+# fun = {'item': 0, 'bottle': 50}
+# winning = [stuff, things, others, fun]
+
+# list_of_keys = ['item', 'bottle']
+
+# print(build_values_list(winning, list_of_keys))
+
+# dict = {}
+
+# stuff = {'stuff': 3}
+# things = {'things': 2}
+# others = {'others': 6}
+# fun = {'item': 0}
+
+# dict.update(stuff)
+# dict.update(things)
+# dict.update(others)
+# dict.update(fun)
+
+# # print(dict)
+
+# # winning = [stuff, things, others, fun]
+
+# item = 'item'
+# stuff = 'stuff'
+# things = 'things'
+# others = 'others'
+
+# big_list = []
+# big_list.append(item)
+# big_list.append(stuff)
+# big_list.append(things)
+
+# # print(big_list)
+# # print(dict)
+
+# print(multiple_listdict(dict, big_list))
+
+# # # print(stuff)
+# # # print(things)
+print(performance())
